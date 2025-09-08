4176679762c22347bf66f0532a7047912b284ffe
Yashas <yashas771981@gmail.com>
2025-09-08 13:31:04 +0530

    Frontend Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default: axios} = require("axios");
 
-export const BASE_URL = "http://localhost:9090"
+export const BASE_URL = "https://careerlink-56f3.onrender.com"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
