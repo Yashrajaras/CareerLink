b37cad6c72ef485c1c245767fe75698f9a147b20
Yashas <yashas771981@gmail.com>
2025-09-08 13:01:33 +0530

    Version 1: Ready for Deployment

---
+++ frontend/src/config/redux/reducer/authReducer/index.js
@@ -0,0 +1,111 @@
+const {createSlice} = require("@reduxjs/toolkit")
+import { getAboutUser, getAllUsers, getConnectionsRequest, getMyConnectionRequests, loginUser, registerUser } from "../../action/authAction"
+
+
+const initialState = {
+    user: undefined,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    loggedIn: false,
+    registered: false,
+    message: "",
+    isTokenThere: false,
+    profileFetched: false,
+    connections: [],
+    connectionRequest: [],
+    all_users: [],
+    all_profiles_fetched: false
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        reset: () => initialState,
+        handleLoginUser: (state) => {
+            state.message = "hello"
+        },
+        emptyMessage: (state) => {
+            state.message = ""
+        },
+        setTokenIsThere: (state) => {
+            state.isTokenThere = true
+        },
+        setTokenIsNotThere: (state) => {
+            state.isTokenThere = false
+        },
+        clearRegistered: (state) => {
+            state.registered = false
+        }
+    },
+
+    extraReducers: (builder) => {
+
+      builder
+        .addCase(loginUser.pending, (state) => {
+            state.isLoading = true
+            state.message = "Knocking the door..."
+        })
+        .addCase(loginUser.fulfilled, (state, action) => {
+
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.loggedIn = true;
+            state.message = "Login is Successful"
+        })
+        .addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload
+        })
+        .addCase(registerUser.pending, (state) => {
+            state.isLoading = true
+            state.message = "Registering you..."
+        })
+        .addCase(registerUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.loggedIn = false;
+            state.registered = true;
+            state.message = "Registration is Successful, Please login";
+        })
+        .addCase(registerUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload
+        })
+        .addCase(getAboutUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.profileFetched = true;
+            state.user = action.payload.profile
+            state.connectionRequest = action.payload.connectionRequest
+        })
+        .addCase(getAllUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.all_profiles_fetched = true;
+            state.all_users = action.payload.profiles
+            console.log(state.all_users)
+        })
+        .addCase(getConnectionsRequest.fulfilled, (state, action) => {
+            state.connections = action.payload
+        })
+        .addCase(getConnectionsRequest.rejected, (state, action) => {
+            state.message = action.payload
+        })
+        .addCase(getMyConnectionRequests.fulfilled, (state, action) => {
+            state.connectionRequest = action.payload
+        })
+        .addCase(getMyConnectionRequests.rejected, (state, action) => {
+            state.message = action.payload
+        })
+    }
+})
+
+export const {reset, emptyMessage, setTokenIsThere, setTokenIsNotThere, clearRegistered} = authSlice.actions;
+
+export default authSlice.reducer
